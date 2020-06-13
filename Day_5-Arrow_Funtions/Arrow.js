8b63d4a76e5aee57820987762d43eba36327636e
Harsh Oza <42001739+HarshOza36@users.noreply.github.com>
2020-06-13 09:14:16 +0530

    Day 5 added

---
+++ Day_5-Arrow_Funtions/Arrow.js
@@ -0,0 +1,18 @@
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    nums = nums.map((num) => (num%2==1? 3:2) * num);
+    return nums;
+}
+
+
+function main() {
+    const n = +(readLine());
+    const a = readLine().split(' ').map(Number);
+    
+    console.log(modifyArray(a).toString().split(',').join(' '));
+}
